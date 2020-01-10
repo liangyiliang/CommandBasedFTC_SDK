9f6257ee4e2f6443f26ead31f9cc73c9ebdde9e2
liangyiliang <liangyiliang@oulook.com>
2020-01-10 14:41:37 -0500

    Made AccessToken abstract so users cannot create an object of it and break encapsulation

--- TeamCode/src/main/java/org/commandftc/AccessToken.java
+++ TeamCode/src/main/java/org/commandftc/AccessToken.java
@@ -4,8 +4,5 @@ package org.commandftc;
  * This class is used to emulate the C++ "friend" class features. 
  * @see https://stackoverflow.com/questions/182278/is-there-a-way-to-simulate-the-c-friend-concept-in-java
  */
-public class AccessToken {
-    public AccessToken() {
-
-    }
+public abstract class AccessToken {
 }
